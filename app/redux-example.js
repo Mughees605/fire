38bc7c996c2c22e59873f4be9245204905bc358c
Hafiz Mughees <mughees605@gmail.com>
2017-02-22 18:19:42 -0800

    adding two new reducers

--- app/redux-example.js
+++ app/redux-example.js
@@ -10,19 +10,8 @@ var reducer = ((state = stateDefault,action)=>{
         case "CHANGE_NAME":
         return {
             ...state,
-            name:action.name
+            hobbies:[action.name]
         };
-        case "ADD_HOBBY":
-        return {
-           ...state,
-           hobbies:[
-               ...state.hobbies,
-               {
-                   hobby:"action.hobby",
-                   id:nextHobbyId++
-               }
-           ]
-        }
         default:
         return state;
 
@@ -33,20 +22,9 @@ var store = redux.createStore(reducer);
 //subcribe 
 store.subscribe(()=>{
     var state = store.getState();
-    console.log(state.name + " state name")
-    console.log(store.getState());
+    console.log(state);
 })
-//
 store.dispatch({
     type:"CHANGE_NAME",
-    name:"Mulla"
-})
-store.dispatch({
-  type:"CHANGE_NAME",
-  name:"Mughees",   
-});
-
-store.dispatch({
-    type:"ADD_HOBBY",
-    hobby:"running"
+    name:"Mughees"
 })
---
+++ app/store/configure.jsx
@@ -0,0 +1,15 @@
+var redux = require('redux');
+var thunk = require('redux-thunk').default;
+var {nameReducer, hobbiesReducer, moviesReducer, mapReducer} = require('./../reducers/index');
+
+export var configure = () => {
+  var reducer = redux.combineReducers({
+    name: nameReducer,
+    hobbies: hobbiesReducer,
+    movies: moviesReducer,
+    map: mapReducer
+  });
+
+  var store = redux.createStore(reducer)
+  return store;
+}
